2ea1fd3dc1cd505873dc08c0e966856bc04c76de
Jiří Strnadel <Juzba88@gmail.com>
2025-08-14 18:23:53 +0200

    save

---
+++ CineTrackBE/ApiControllers/FilmApiController.cs
@@ -0,0 +1,53 @@
+﻿using CineTrackBE.Models.Entities;
+using CineTrackBE.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CineTrackBE.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmApiController(IRepository<Film> filmRepository) : ControllerBase
+    {
+        private readonly IRepository<Film> _filmRepository = filmRepository;
+
+
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            return await _filmRepository.GetList().Select(p => p.Name).ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<IEnumerable<string>> GetFilms()
+        {
+            return await _filmRepository.GetList().Select(p => p.Name).ToListAsync();
+        }
+
+        // GET api/<FilmApiController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FilmApiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<FilmApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FilmApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
