d16e1140ba36284761c7cc61acd942c870d629a3
Jiří Strnadel <Juzba88@gmail.com>
2025-08-19 21:32:02 +0200

    film search done

--- CineTrackBE/ApiControllers/FilmApiController.cs
+++ CineTrackBE/ApiControllers/FilmApiController.cs
@@ -6,131 +6,140 @@ using CineTrackFE.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace CineTrackBE.ApiControllers
+namespace CineTrackBE.ApiControllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FilmApiController(IRepository<Film> filmRepository, IRepository<Genre> genreRepository, IDataService dataService) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FilmApiController(IRepository<Film> filmRepository, IRepository<Genre> genreRepository, IDataService dataService) : ControllerBase
+    private readonly IRepository<Film> _filmRepository = filmRepository;
+    private readonly IRepository<Genre> _genreRepository = genreRepository;
+    private readonly IDataService _dataService = dataService;
+
+
+    // Top 5 Latest Films //
+    [HttpGet]
+    [Route("LatestFilms")]
+    public async Task<ActionResult<IEnumerable<FilmDto>>> GetLatestFilms()
     {
-        private readonly IRepository<Film> _filmRepository = filmRepository;
-        private readonly IRepository<Genre> _genreRepository = genreRepository;
-        private readonly IDataService _dataService = dataService;
+        var films = await _filmRepository.GetList().OrderByDescending(p => p.ReleaseDate).Take(5).ToListAsync();
 
+        if (films == null || films.Count == 0) return NotFound();
 
-        // Top 5 Latest Films //
-        [HttpGet]
-        [Route("LatestFilms")]
-        public async Task<ActionResult<IEnumerable<FilmDto>>> GetLatestFilms()
+        var filmsDTO = films.Select(p => new FilmDto()
         {
-            var films = await _filmRepository.GetList().OrderByDescending(p => p.ReleaseDate).Take(5).ToListAsync();
+            Id = p.Id,
+            Name = p.Name,
+            Director = p.Director,
+            ImageFileName = p.ImageFileName,
+            Description = p.Description,
+            ReleaseDate = p.ReleaseDate,
+            Genres = [.. p.FilmGenres.Select(g => g.Genre.Name)]
+        });
 
-            if (films == null || films.Count == 0) return NotFound();
+        return Ok(filmsDTO);
+    }
 
-            var filmsDTO = films.Select(p => new FilmDto()
-            {
-                Id = p.Id,
-                Name = p.Name,
-                Director = p.Director,
-                ImageFileName = p.ImageFileName,
-                Description = p.Description,
-                ReleaseDate = p.ReleaseDate,
-                Genres = [.. p.FilmGenres.Select(g => g.Genre.Name)]
-            });
 
-            return Ok(filmsDTO);
+    // Get all genres //
+    [HttpGet]
+    [Route("AllGenres")]
+    public async Task<ActionResult<IEnumerable<GenreDto>>> GetAllGenres()
+    {
+        var genres = await _genreRepository.GetList().ToListAsync();
+
+        if (genres == null || genres.Count == 0) return NotFound();
+
+        var genresDto = genres.Select(p => new GenreDto()
+        {
+            Id = p.Id,
+            Name = p.Name
+        });
+
+        return Ok(genresDto);
+    }
+
+
+    [HttpPost]
+    [Route("CatalogSearch")]
+    public async Task<ActionResult<IEnumerable<Film>>> CatalogPost([FromBody] SearchParametrsDto? searchParams)
+    {
+        // get film list from db
+        var films = _dataService.GetFilmListAsync_InclFilmGenres();
+
+        // search by name
+        if (!string.IsNullOrWhiteSpace(searchParams?.SearchText))
+        {
+            films = films.Where(p => p.Name.Contains(searchParams.SearchText));
         }
 
-
-        // Get all genres //
-        [HttpGet]
-        [Route("AllGenres")]
-        public async Task<ActionResult<IEnumerable<GenreDto>>> GetAllGenres()
+        // search by release date
+        if (searchParams?.SearchByYear != null && searchParams.SearchByYear > 0)
         {
-            var genres = await _genreRepository.GetList().ToListAsync();
-
-            if (genres == null || genres.Count == 0) return NotFound();
-
-            var genresDto = genres.Select(p => new GenreDto()
-            {
-                Id = p.Id,
-                Name = p.Name
-            });
-
-            return Ok(genresDto);
+            films = films.Where(p => p.ReleaseDate.Year == searchParams.SearchByYear);
         }
 
-
-        [HttpPost]
-        [Route("CatalogSearch")]
-        public async Task<ActionResult<IEnumerable<Film>>> CatalogPost([FromBody] SearchParametrsDto? searchParams)
+        // search by genre
+        if (searchParams?.GenreId != null && searchParams.GenreId > 0)
         {
-
-            if (searchParams == null)
-            {
-
-                var films = await _filmRepository.GetList().OrderByDescending(p => p.Id).ToListAsync();
-
-                if (films == null || films.Count == 0) return NotFound();
-
-                var filmsDTO = films.Select(p => new FilmDto()
-                {
-                    Id = p.Id,
-                    Name = p.Name,
-                    Director = p.Director,
-                    ImageFileName = p.ImageFileName,
-                    Description = p.Description,
-                    ReleaseDate = p.ReleaseDate,
-                    Genres = [.. p.FilmGenres.Select(g => g.Genre.Name)]
-                });
-
-                return Ok(filmsDTO);
-
-
-            }
-            else
-            {
-
-                var films = _dataService.GetFilmListAsync_InclFilmGenres().Where(p => p.Name.Contains(searchParams.SearchText ?? ""));
-
-                if (searchParams.SearchByYear != null && searchParams.SearchByYear > 0)
-                {
-                    films = films.Where(p => p.ReleaseDate.Year == searchParams.SearchByYear);
-                }
-
-
-
-                //if (searchParams.GenreId != null && searchParams.GenreId > 0)
-                //{
-                //    films = films.Where(p => p.FilmGenres);
-                //}
-
-
-
-
-
-                return Ok();
-            }
+            films = films.Where(p => p.FilmGenres.Any(p => p.GenreId == searchParams.GenreId));
         }
 
-
-
-        // GET api/<FilmApiController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // order
+        switch (searchParams?.SearchOrder)
         {
-            return "value";
+            case ("NameDesc"):
+                films = films.OrderByDescending(p => p.Name);
+                break;
+            case ("NameAsc"):
+                films = films.OrderBy(p => p.Name);
+                break;
+            case ("YearDesc"):
+                films = films.OrderByDescending(p => p.ReleaseDate);
+                break;
+            case ("YearAsc"):
+                films = films.OrderBy(p => p.ReleaseDate);
+                break;
+            default:
+                films = films.OrderByDescending(p => p.Id);
+                break;
         }
 
-        // PUT api/<FilmApiController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        var searchResult = await films.ToListAsync();
 
-        // DELETE api/<FilmApiController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // data transfer object
+        var filmsDTO = searchResult.Select(p => new FilmDto()
         {
-        }
+            Id = p.Id,
+            Name = p.Name,
+            Director = p.Director,
+            ImageFileName = p.ImageFileName,
+            Description = p.Description,
+            ReleaseDate = p.ReleaseDate,
+            Genres = [.. p.FilmGenres.Select(g => g.Genre.Name)]
+        });
+
+        return Ok(filmsDTO);
+    }
+
+
+
+    // GET api/<FilmApiController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // PUT api/<FilmApiController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<FilmApiController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
