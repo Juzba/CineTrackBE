0b162ceff6ad2f11b7c8517631ba6de95615a1d8
Jiří Strnadel <Juzba88@gmail.com>
2025-09-03 19:22:04 +0200

    update mock

--- CineTrackBE.Tests/Helpers/MockHelper.cs
+++ CineTrackBE.Tests/Helpers/MockHelper.cs
@@ -6,23 +6,23 @@ namespace CineTrackBE.Tests.Helpers
 {
     public class MockHelper
     {
-        //public static Mock<ILogger<T>> CreateLogger<T>() => new Mock<ILogger<T>>();
+        public static Mock<ILogger<T>> CreateLogger<T>() => new Mock<ILogger<T>>();
 
-        //public static ILogger<T> CreateLoggerObject<T>() => Mock.Of<ILogger<T>>();
+        public static ILogger<T> CreateLoggerObject<T>() => Mock.Of<ILogger<T>>();
 
-        //public static Mock<IConfiguration> CreateConfiguration(Dictionary<string, string> settings = null!)
-        //{
-        //    var config = new Mock<IConfiguration>();
+        public static Mock<IConfiguration> CreateConfiguration(Dictionary<string, string> settings = null!)
+        {
+            var config = new Mock<IConfiguration>();
 
-        //    if (settings != null)
-        //    {
-        //        foreach (var setting in settings)
-        //        {
-        //            config.Setup(x => x[setting.Key]).Returns(setting.Value);
-        //        }
-        //    }
+            if (settings != null)
+            {
+                foreach (var setting in settings)
+                {
+                    config.Setup(x => x.GetValue(typeof(string), setting.Key)).Returns(setting.Value);
+                }
+            }
 
-        //    return config;
-        //}
+            return config;
+        }
     }
 }
